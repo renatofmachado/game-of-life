6f240fd4484ba7c86c69e0ec1ed66a65e0334979
Renato Machado <renato.machado@talkdesk.com>
2024-03-27 15:16:36 +0000

    Set new TPS to slow updates

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"flag"
-	"log"
+  "flag"
+  "log"
 
-	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/renatofmachado/game-of-life/pkg"
+  "github.com/hajimehoshi/ebiten/v2"
+  "github.com/renatofmachado/game-of-life/pkg"
 )
 
 const (
@@ -14,14 +14,15 @@ const (
 )
 
 var (
-	random = flag.Int("random", -1, "How much of a chance there is of generating random life. 0-100")
+  random = flag.Int("random", -1, "How much of a chance there is of generating random life. 0-100")
 )
 
 func main() {
   flag.Parse()
 
-	ebiten.SetWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT)
-	ebiten.SetWindowTitle("Game of Life")
+  ebiten.SetWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT)
+  ebiten.SetWindowTitle("Game of Life")
+  ebiten.SetTPS(20)
 
   game := pkg.NewGame(WINDOW_WIDTH, WINDOW_HEIGHT)
 
@@ -29,7 +30,7 @@ func main() {
     game.SeedRandomLife(*random)
   }
 
-	if err := ebiten.RunGame(game); err != nil {
-		log.Fatal(err)
-	}
+  if err := ebiten.RunGame(game); err != nil {
+    log.Fatal(err)
+  }
 }
