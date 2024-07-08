9cd1ddc0469ee67537a74e13df01a41fef4abb72
Renato Machado <renato.machado@talkdesk.com>
2024-07-08 12:49:41 +0100

    chore: wip

--- main.go
+++ main.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-  "flag"
-  "log"
+	"flag"
+	"log"
 
-  "github.com/hajimehoshi/ebiten/v2"
-  "github.com/renatofmachado/game-of-life/pkg"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/renatofmachado/game-of-life/pkg"
 )
 
 const (
-  WINDOW_WIDTH = 800
-  WINDOW_HEIGHT = 600
+	WINDOW_WIDTH  = 800
+	WINDOW_HEIGHT = 600
 )
 
 var (
-  random = flag.Int("random", -1, "How much of a chance there is of generating random life. 0-100")
+	random = flag.Int("random", -1, "How much of a chance there is of generating random life. 0-100")
 )
 
 func main() {
-  flag.Parse()
+	flag.Parse()
 
-  ebiten.SetWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT)
-  ebiten.SetWindowTitle("Game of Life")
-  ebiten.SetTPS(20)
+	ebiten.SetWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT)
+	ebiten.SetWindowTitle("Game of Life")
+	ebiten.SetTPS(20)
 
-  game := pkg.NewGame(WINDOW_WIDTH, WINDOW_HEIGHT)
+	game := pkg.NewGame(WINDOW_WIDTH, WINDOW_HEIGHT)
 
-  if *random > 0 && *random <= 100 {
-    game.SeedRandomLife(*random)
-  }
+	if *random > 0 && *random <= 100 {
+		game.SeedRandomLife(*random)
+	}
 
-  if err := ebiten.RunGame(game); err != nil {
-    log.Fatal(err)
-  }
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
 }
--- pkg/game.go
+++ pkg/game.go
@@ -10,182 +10,182 @@ import (
 
 // 8 neighbors coordinates expressed as [x, y]
 var directions = [][]int{
-  {-1, -1},
-  {0, -1},
-  {1, -1},
-  {-1, 0},
-  {1, 0},
-  {-1, 1},
-  {0, 1},
-  {1, 1},
+	{-1, -1},
+	{0, -1},
+	{1, -1},
+	{-1, 0},
+	{1, 0},
+	{-1, 1},
+	{0, 1},
+	{1, 1},
 }
 
-type Game struct{
-  cells [][]bool
-  cellSize int
-  screenW int
-  width int
-  screenH int
-  height int
-  interactive bool
-  running bool
+type Game struct {
+	cells       [][]bool
+	cellSize    int
+	screenW     int
+	width       int
+	screenH     int
+	height      int
+	interactive bool
+	running     bool
 }
 
 func getCellsLimits(width, height, cellSize int) (int, int) {
-  return width / cellSize, height / cellSize
+	return width / cellSize, height / cellSize
 }
 
 func NewGame(width, height int) *Game {
-  initialCellSize := 5
-  w, h := getCellsLimits(width, height, initialCellSize)
-  
-  cells := make([][]bool, h)
-  for i := range cells {
-    cells[i] = make([]bool, w)
-  }
+	initialCellSize := 5
+	w, h := getCellsLimits(width, height, initialCellSize)
 
-  return &Game{
-    cells: cells,
-    cellSize: initialCellSize,
-    screenW: width,
-    width: w,
-    screenH: height,
-    height: h,
-    interactive: false,
-    running: true,
-  }
+	cells := make([][]bool, h)
+	for i := range cells {
+		cells[i] = make([]bool, w)
+	}
+
+	return &Game{
+		cells:       cells,
+		cellSize:    initialCellSize,
+		screenW:     width,
+		width:       w,
+		screenH:     height,
+		height:      h,
+		interactive: false,
+		running:     true,
+	}
 }
 
 func (g *Game) SeedLife(x, y int) {
-  if x < 0 || x >= g.width || y < 0 || y >= g.height {
-    return
-  }
+	if x < 0 || x >= g.width || y < 0 || y >= g.height {
+		return
+	}
 
-  g.cells[y][x] = true
+	g.cells[y][x] = true
 
-  for _, coordinates := range directions {
-    neighborX := x + coordinates[0]
-    neighborY := y + coordinates[1]
+	for _, coordinates := range directions {
+		neighborX := x + coordinates[0]
+		neighborY := y + coordinates[1]
 
-    if neighborX < 0 || neighborX >= g.width || neighborY < 0 || neighborY >= g.height {
-      continue
-    }
+		if neighborX < 0 || neighborX >= g.width || neighborY < 0 || neighborY >= g.height {
+			continue
+		}
 
-    if rand.Intn(100) <= 50 {
-      g.cells[neighborY][neighborX] = true
-    }
-  }
+		if rand.Intn(100) <= 50 {
+			g.cells[neighborY][neighborX] = true
+		}
+	}
 }
 
 func (g *Game) SeedRandomLife(chance int) {
-  for i := 0; i < g.height / 2; i++ {
-    if rand.Intn(100) <= chance {
-      randomX := rand.Intn(g.width)
-      randomY := rand.Intn(g.height)
-      g.SeedLife(randomX, randomY)
-    }
-  }
+	for i := 0; i < g.height/2; i++ {
+		if rand.Intn(100) <= chance {
+			randomX := rand.Intn(g.width)
+			randomY := rand.Intn(g.height)
+			g.SeedLife(randomX, randomY)
+		}
+	}
 }
 
 func countNeighbors(cells [][]bool, x, y int) int {
-  count := 0
-  
-  for _, coordinates := range directions {
-    neighborX := x + coordinates[0]
-    neighborY := y + coordinates[1]
+	count := 0
 
-    if neighborX >= 0 && neighborX < len(cells[0]) && neighborY >= 0 && neighborY < len(cells) {
-      if cells[neighborY][neighborX] {
-        count++
-      }
-    }
-  }
+	for _, coordinates := range directions {
+		neighborX := x + coordinates[0]
+		neighborY := y + coordinates[1]
 
-  return count
+		if neighborX >= 0 && neighborX < len(cells[0]) && neighborY >= 0 && neighborY < len(cells) {
+			if cells[neighborY][neighborX] {
+				count++
+			}
+		}
+	}
+
+	return count
 }
 
 func (g *Game) nextGeneration() {
-  nextGen := make([][]bool, g.height)
-  for i := range nextGen {
-    nextGen[i] = make([]bool, g.width)
-  }
+	nextGen := make([][]bool, g.height)
+	for i := range nextGen {
+		nextGen[i] = make([]bool, g.width)
+	}
 
-  for y := range g.cells {
-    for x := range g.cells[y] {
-      // Rule 2
-      nextGen[y][x] = g.cells[y][x]
+	for y := range g.cells {
+		for x := range g.cells[y] {
+			// Rule 2
+			nextGen[y][x] = g.cells[y][x]
 
-      neighborsCount := countNeighbors(g.cells, x, y)
+			neighborsCount := countNeighbors(g.cells, x, y)
 
-      // Rule 1 and 3
-      if neighborsCount < 2 || neighborsCount > 3 {
-        nextGen[y][x] = false
-      }
+			// Rule 1 and 3
+			if neighborsCount < 2 || neighborsCount > 3 {
+				nextGen[y][x] = false
+			}
 
-      // Rule 2 and 4
-      if neighborsCount == 3 {
-        nextGen[y][x] = true
-      }
-    }
-  }
+			// Rule 2 and 4
+			if neighborsCount == 3 {
+				nextGen[y][x] = true
+			}
+		}
+	}
 
-  g.cells = nextGen
+	g.cells = nextGen
 }
 
 func (g *Game) setNewCellsSize() {
-  w, h := getCellsLimits(g.screenW, g.screenH, g.cellSize)
+	w, h := getCellsLimits(g.screenW, g.screenH, g.cellSize)
 
-  newCells := make([][]bool, h)
-  for i := range newCells {
-    newCells[i] = make([]bool, w)
-    if i < len(g.cells) {
-      for j := range newCells[i] {
-        if j < len(g.cells[i]) {
-          newCells[i][j] = g.cells[i][j]
-        }
-      }
-    }
-  }
+	newCells := make([][]bool, h)
+	for i := range newCells {
+		newCells[i] = make([]bool, w)
+		if i < len(g.cells) {
+			for j := range newCells[i] {
+				if j < len(g.cells[i]) {
+					newCells[i][j] = g.cells[i][j]
+				}
+			}
+		}
+	}
 
-  g.cells = newCells
-  g.width = w
-  g.height = h
+	g.cells = newCells
+	g.width = w
+	g.height = h
 }
 
 func (g *Game) Reset() {
-  g.cells = make([][]bool, g.height)
-  for i := range g.cells {
-    g.cells[i] = make([]bool, g.width)
-  }
+	g.cells = make([][]bool, g.height)
+	for i := range g.cells {
+		g.cells[i] = make([]bool, g.width)
+	}
 }
 
 func (g *Game) Update() error {
-  RegisterIO(g)
+	RegisterIO(g)
 
-  if !g.running {
-    return nil
-  }
+	if !g.running {
+		return nil
+	}
 
 	g.nextGeneration()
 
-  return nil
+	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-  for y := range g.cells {
-    for x := range g.cells[y] {
-      if g.cells[y][x] {
-        posX := float32(x * g.cellSize)
-        posY := float32(y * g.cellSize)
-        cellSize := float32(g.cellSize)
-        vector.DrawFilledRect(screen, posX, posY, cellSize, cellSize, color.White, true) }
-      }
-  }
+	for y := range g.cells {
+		for x := range g.cells[y] {
+			if g.cells[y][x] {
+				posX := float32(x * g.cellSize)
+				posY := float32(y * g.cellSize)
+				cellSize := float32(g.cellSize)
+				vector.DrawFilledRect(screen, posX, posY, cellSize, cellSize, color.White, true)
+			}
+		}
+	}
 
-  DrawHud(g, screen)
+	DrawHud(g, screen)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
 	return outsideWidth, outsideHeight
 }
-
